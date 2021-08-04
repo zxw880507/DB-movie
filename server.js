2f9c78cfff88a1aa079550c39d216840b9017b4e
Calvin <zxw880507@gmail.com>
2021-08-04 15:17:16 -0600

    setup environmental variable

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+require("dotenv").config();
+
+const api = process.env.api_key;
 
 const { json, urlencoded } = express;
 const { join } = require("path");
@@ -11,4 +14,5 @@ app.use(express.static(join(__dirname, "public")));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  console.log(api);
 });
