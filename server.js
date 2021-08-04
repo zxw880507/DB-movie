81dce645d62be1d8af45c6969329fe3410d380eb
Calvin <zxw880507@gmail.com>
2021-08-04 14:47:33 -0600

    server setup

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("aaa");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const { json, urlencoded } = express;
+const { join } = require("path");
+
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(express.static(join(__dirname, "public")));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
