dd92bd946993dc641560234181cac433fd127130
Calvin <zxw880507@gmail.com>
2021-08-05 12:41:27 -0600

    create cache promise

---
+++ cache.js
@@ -0,0 +1,27 @@
+// init cache nodule
+const NodeCache = require("node-cache");
+const myCache = new NodeCache();
+
+module.exports = {
+  get(key) {
+    return new Promise((resolve, reject) => {
+      const response = {};
+      response.data = myCache.get(key);
+      resolve(response);
+    });
+  },
+
+  set(key, value, ttl) {
+    return new Promise((resolve, reject) => {
+      const success = myCache.set(key, value, ttl);
+      console.log("success:", success);
+      if (success) {
+        const res = { msg: "Cache stored successfully!" };
+        resolve(res);
+      } else {
+        const err = { msg: "Cache failed to save!" };
+        reject(err);
+      }
+    });
+  },
+};
--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
-// init cache nodule
-const NodeCache = require("node-cache");
-const myCache = new NodeCache();
+const cache = require("./cache");
 
 // express middleware
 const { json, urlencoded } = express;
@@ -16,9 +13,22 @@ app.use(urlencoded({ extended: false }));
 app.use(express.static(join(__dirname, "public")));
 
 //api routes
-const api = require("./src/routes/index");
-app.use("/api", api);
+// const api = require("./src/routes/index");
+// app.use("/api", api);
 
+app.use("/cache/:username", (request, response) => {
+  const { username } = request.params;
+  cache.get("username").then((res) => {
+    if (res.data) {
+      console.log(res.data);
+    } else {
+      cache
+        .set("username", username, 60000)
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
