b5c682f4bc7ac175e4d191bcbe3d888b653bf89c
Calvin <zxw880507@gmail.com>
2021-08-05 14:17:42 -0600

    add server folder

--- src/routes/ping.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-module.exports = (api_key = null) => {
-  router.get("/", (req, res) => {
-    res.status(200).json({ success: true, key: api_key });
-  });
-  return router;
-};
