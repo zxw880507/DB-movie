4e21cdeea0b57f180abe33304eea249494f444b3
Calvin <zxw880507@gmail.com>
2021-08-06 10:44:50 -0600

    add appbar

--- client/src/components/Navbar/BarContent.js
+++ client/src/components/Navbar/BarContent.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { Toolbar, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles({
+  container: {
+    width: "70%",
+    padding: 0
+  },
+});
 export default function BarContent() {
+  const classes = useStyles();
+
   return (
-    <Toolbar>
+    <Toolbar className={classes.container}>
       <Typography variant="h6">Scroll to Hide App Bar</Typography>
     </Toolbar>
   );
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,12 +3,19 @@ import AppBar from "@material-ui/core/AppBar";
 import BarContent from "./BarContent";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import Slide from "@material-ui/core/Slide";
+import { makeStyles } from "@material-ui/styles";
 
+const useStyles = makeStyles({
+  container: {
+    alignItems: "center"
+  },
+});
 export default function HideOnScroll(props) {
+  const classes = useStyles();
   const trigger = useScrollTrigger();
   return (
     <Slide in={!trigger}>
-      <AppBar>
+      <AppBar className={classes.container}>
         <BarContent />
       </AppBar>
     </Slide>
