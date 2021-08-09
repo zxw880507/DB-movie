8a68adaa9a2239cf49f408e4c88889d980648a95
Calvin <zxw880507@gmail.com>
2021-08-09 17:48:24 -0600

    init scroll list

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, makeStyles, Toolbar } from "@material-ui/core";
 import Header from "./Header";
+import Swiper from "./Swiper";
 
 const useStyles = makeStyles({
   root: {
@@ -14,6 +15,7 @@ export default function Main() {
     <Container maxWidth="lg" className={classes.root}>
       <Toolbar />
       <Header />
+      <Swiper />
     </Container>
   );
 }
