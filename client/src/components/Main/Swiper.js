bef48d9f91be3c431c726439165d233fdd689c99
Calvin <zxw880507@gmail.com>
2021-08-10 16:23:07 -0600

    complete scrollbar event

--- client/src/components/Main/Swiper.js
+++ client/src/components/Main/Swiper.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import data from "./data.json";
 
 export default function Swiper() {
+  // const overflow = useRef(null);
+  const barContainer = useRef(null);
+  const [PosisionX, setPositionX] = useState(0);
+  console.log("PosisionX:", PosisionX);
   return (
     <>
       <div
@@ -17,26 +21,56 @@ export default function Swiper() {
             display: "flex",
             position: "absolute",
             top: 0,
-            left: 0
+            left: 0,
           }}
+          // ref={overflow}
         >
-          {data.map((source) => (
+          {data.map((source, index) => (
             <img
+              key={index}
               src={`https://image.tmdb.org/t/p/w500${source.poster_path}`}
               alt=""
             />
           ))}
         </div>
       </div>
-      <div style={{ height: 20, backgroundColor: "rgba(0,0,0,.2)" }}>
+      <div
+        style={{
+          position: "relative",
+          height: 20,
+          backgroundColor: "rgba(0,0,0,.2)",
+        }}
+        ref={barContainer}
+      >
         <span
           style={{
-            display: "block",
+            position: "absolute",
+            top: 0,
+            left: PosisionX,
             width: "20%",
-            height: "90%",
+            height: "100%",
             backgroundColor: "rgba(0,0,0,.8)",
           }}
-          onScroll={(e) => {}}
+          onMouseDown={(e) => {
+            const outerX = barContainer.current.offsetLeft;
+            const x = e.pageX - outerX - e.target.offsetLeft;
+            const maxPos =
+              barContainer.current.offsetWidth - e.target.offsetWidth;
+            const move = (e) => {
+              let newPositionX = e.pageX - outerX - x;
+              if (newPositionX > maxPos) {
+                newPositionX = maxPos;
+              }
+              if (newPositionX < 0) {
+                newPositionX = 0;
+              }
+              setPositionX(newPositionX);
+            };
+            document.addEventListener("mousemove", move);
+            document.addEventListener("mouseup", () => {
+              document.removeEventListener("mousemove", move);
+            });
+          }}
         />
       </div>
     </>
