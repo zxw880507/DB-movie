f1da3f178823f5baa4a42ea33b7bcbae38242d3e
Calvin <zxw880507@gmail.com>
2021-08-13 14:37:35 -0600

    import helpers

--- client/src/components/Main/Swiper/TabItem.js
+++ client/src/components/Main/Swiper/TabItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classNames from "classnames";
+import { tagFormatting } from "../../../helpers";
 
 export default function TabItem(props) {
   const { selected, value, onChange } = props;
@@ -9,7 +10,7 @@ export default function TabItem(props) {
   });
   return (
     <li className={tabSelectedClass} onClick={() => onChange(value)}>
-      <p>{value}</p>
+      <p>{tagFormatting(value)}</p>
     </li>
   );
 }
--- client/src/components/Main/Swiper/TabItemList.js
+++ client/src/components/Main/Swiper/TabItemList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import TabItem from "./TabItem";
 
 export default function TabItemList(props) {
