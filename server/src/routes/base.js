cfba0beb70bb25eb310d491af1f950bdd94cffc2
Calvin <zxw880507@gmail.com>
2021-08-13 15:19:03 -0600

    add a trending api routes

--- server/src/routes/base.js
+++ server/src/routes/base.js
@@ -2,15 +2,40 @@ const router = require("express").Router();
 const axios = require("axios");
 const cache = require("../cache");
 
-
 module.exports = (api_key, helpers) => {
+  router.get("/trending/:type/:time_window", (request, response) => {
+    const { type, time_window } = request.params;
+    const cacheKey = helpers.generateCacheKey({
+      trending: true,
+      ...request.params,
+    });
+
+    cache.get(cacheKey).then((res) => {
+      if (res.data) {
+        console.log({ msg: `${cacheKey} fetched from cache!` });
+        response.json(res.data);
+      } else {
+        const url = `https://api.themoviedb.org/3/trending/${type}/${time_window}?api_key=${api_key}&language=en-US&page=1`;
+        axios
+          .get(url)
+          .then((res) => {
+            response.json(res.data);
+            cache
+              .set(cacheKey, res.data, 10000)
+              .then((res) => console.log(res))
+              .catch((err) => console.log(err));
+          })
+          .catch(() => response.status(404));
+      }
+    });
+  });
   router.get("/:type/:keyword", (request, response) => {
     const { type, keyword } = request.params;
     const cacheKey = helpers.generateCacheKey(request.params);
 
     cache.get(cacheKey).then((res) => {
       if (res.data) {
-        console.log({ msg: "keyword fetched from cache!" });
+        console.log({ msg: `${cacheKey} fetched from cache!` });
         response.json(res.data);
       } else {
         const url = `https://api.themoviedb.org/3/${type}/${keyword}?api_key=${api_key}&language=en-US&page=1`;
@@ -36,7 +61,7 @@ module.exports = (api_key, helpers) => {
 
     cache.get(cacheKey).then((res) => {
       if (res.data) {
-        console.log({ msg: "data fetched from cache!" });
+        console.log({ msg: `${cacheKey} fetched from cache!` });
         response.json(res.data);
       } else {
         const base_URL = `https://api.themoviedb.org/3/discover/${type}?`;
