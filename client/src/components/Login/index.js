e2c1866aa91598ff589d5ce80c0c92fcce9ec34a
Calvin <zxw880507@gmail.com>
2021-08-14 14:40:30 -0600

    create login component

---
+++ client/src/components/Login/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./login.css";
+
+export default function Login() {
+  return (
+    <div className="login-container">
+      <div className="login-heading-box">
+        <h1 className="login-heading-title">WELCOME TO</h1>
+        <p>DB MOVIE</p>
+      </div>
+      <div className="login-content-box"></div>
+    </div>
+  );
+}
