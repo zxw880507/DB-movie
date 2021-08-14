bca929b1da29b43a53a8bf32fcdabb2548180cbb
Calvin <zxw880507@gmail.com>
2021-08-14 14:40:08 -0600

    add css shades

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 // import './App.css';
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
+import Login from "./components/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <>
-      <Navbar />
-      <Main />
+      <Login />
+      <div style={{ opacity: 0.5, backgroundColor: "#000" }}>
+        <Navbar />
+        <Main />
+      </div>
     </>
   );
 }
