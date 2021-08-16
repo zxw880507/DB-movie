ea63158bc27b62f359d92275df9b82a759df9ab8
Calvin <zxw880507@gmail.com>
2021-08-16 13:24:49 -0600

    add register form

---
+++ client/src/components/Login/RegisterForm.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+export default function RegisterForm() {
+  return (
+    <Form className="form-card">
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">Email is invalid</Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+        <Form.Text className="text-muted">Password is incorrect</Form.Text>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Re-Password</Form.Label>
+        <Form.Control type="password" placeholder="Re-Password" />
+        <Form.Text className="text-muted">Password is incorrect</Form.Text>
+      </Form.Group>
+      <Button variant="primary" type="submit" className="form-submit-button">
+        Submit
+      </Button>
+    </Form>
+  );
+}
