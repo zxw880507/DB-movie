8388e430f9754b2a6d25e8401f50033931a7cb73
Calvin <zxw880507@gmail.com>
2021-08-16 21:11:10 -0600

    alter register form

--- client/src/components/Login/RegisterForm.js
+++ client/src/components/Login/RegisterForm.js
@@ -3,22 +3,34 @@ import { Form, Button } from "react-bootstrap";
 export default function RegisterForm() {
   return (
     <Form className="form-card">
-      <Form.Group className="mb-3" controlId="register-email">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+      <Form.Floating className="mb-3 control-register">
+        <Form.Control
+          id="floatingInputCustom"
+          type="email"
+          placeholder="name@example.com"
+        />
+        <label htmlFor="floatingInputCustom">Email address</label>
         <Form.Text className="text-muted">Email is invalid</Form.Text>
-      </Form.Group>
+      </Form.Floating>
+      <Form.Floating className="mb-3 control-register">
+        <Form.Control
+          id="floatingPasswordCustom"
+          type="password"
+          placeholder="Password"
+        />
+        <label htmlFor="floatingPasswordCustom">Password</label>
+        <Form.Text className="text-muted">Password is incorrect</Form.Text>
+      </Form.Floating>
+      <Form.Floating className="mb-3 control-register">
+        <Form.Control
+          id="floatingRePasswordCustom"
+          type="Re-password"
+          placeholder="Re-Password"
+        />
+        <label htmlFor="floatingPasswordCustom">Re-Password</label>
+        <Form.Text className="text-muted">Password is incorrect</Form.Text>
+      </Form.Floating>
 
-      <Form.Group className="mb-3" controlId="register-password">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-        <Form.Text className="text-muted">Password is incorrect</Form.Text>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="retype-register-password">
-        <Form.Label>Re-Password</Form.Label>
-        <Form.Control type="password" placeholder="Re-Password" />
-        <Form.Text className="text-muted">Password is incorrect</Form.Text>
-      </Form.Group>
       <Button variant="primary" type="submit" className="form-submit-button">
         Create account
       </Button>
