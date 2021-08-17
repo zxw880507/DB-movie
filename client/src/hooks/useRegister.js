c752b8ea0b45a5ee674ef42f40c5dd3b0a68a331
Calvin <zxw880507@gmail.com>
2021-08-17 17:22:32 -0600

    add register hook

---
+++ client/src/hooks/useRegister.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+export default function useRegister() {
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+    repassword: "",
+  });
+  const [isWarning, setIswarning] = useState({
+    email: null,
+    password: null,
+    repassword: null,
+  });
+  const changeInput = (e) => {
+    setInput((prev) => ({ ...prev, [e.target.dataset.type]: e.target.value }));
+  };
+
+  const checkIsWarning = () => {
+    const setByRegex = (regex, type) => {
+      setIswarning((prev) => ({
+        ...prev,
+        [type]: !regex.test(input[type]),
+      }));
+    };
+    for (let type in input) {
+      switch (type) {
+        case "email":
+          setByRegex(/^\S+@\S+\.\S+$/, type);
+          break;
+        case "password":
+          setByRegex(/^.{6,}/, type);
+          break;
+        case "repassword":
+          setByRegex(new RegExp(`^${input.password}$`), type);
+          break;
+        default:
+          break;
+      }
+    }
+  };
+  const signUp = (e) => {
+    e.preventDefault();
+    checkIsWarning();
+    if (Object.keys(isWarning).every((key) => isWarning[key] === false)) {
+      console.log("submit successfully");
+    }
+  };
+  return { input, isWarning, changeInput, checkIsWarning, signUp };
+}
