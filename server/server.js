8889a3aeb8b96129863f41959377d4d511707ba4
Calvin <zxw880507@gmail.com>
2021-08-17 17:50:09 -0600

    app use user route

--- server/server.js
+++ server/server.js
@@ -15,7 +15,8 @@ app.use(express.static(join(__dirname, "public")));
 // api routes
 const api = require("./src/routes/index");
 app.use("/api", api);
-
+//user routes
+app.use("/user", require("./src/routes/user"));
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
