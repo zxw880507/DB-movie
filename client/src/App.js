312904686ef2936fb9eff37af5f70146fe8fa05b
Calvin <zxw880507@gmail.com>
2021-08-18 16:56:56 -0600

    check cookie for login

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import Login from "./components/Login";
 
 function App() {
   const [showLogin, setShowLogin] = useState(false);
-  const [isAuth, setIsAuth] = useState(true);
+  const [isAuth, setIsAuth] = useState(false);
 
-  console.log(isAuth, showLogin);
+  useEffect(() => {
+    axios.get("/user/login").then(() => setIsAuth(true));
+  }, []);
+  // console.log(isAuth, showLogin);
   const openLogin = () => {
     setShowLogin(true);
   };
