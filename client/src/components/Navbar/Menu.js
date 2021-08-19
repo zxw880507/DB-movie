3fa062a9660479760a443e594282b33e76ba4c95
Calvin <zxw880507@gmail.com>
2021-08-19 11:09:52 -0600

    add style for custom menu

--- client/src/components/Navbar/Menu.js
+++ client/src/components/Navbar/Menu.js
@@ -1,56 +1,91 @@
-import React, { useState } from "react";
-import { Button, Menu, MenuItem, makeStyles } from "@material-ui/core";
+import React, { useState, useRef, useEffect } from "react";
+import { Button, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
   button: {
-    margin: "0 20px",
+    margin: "0 1.5em",
     color: "#fff",
     borderColor: "#fff",
     opacity: 0.7,
+    fontSize: "1em",
+    textTransform: "lowercase",
     "&:hover": {
       opacity: 1,
     },
   },
+  menuContainer: {
+    position: "absolute",
+    top: "120%",
+    left: "50%",
+    transform: "translateX(-50%)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    backgroundColor: "rgba(13, 37, 63, .1)",
+    whiteSpace: "nowrap",
+    boxShadow:
+      "inset 1px 1px 2em rgba(0,0,0,1), 0px 0px 0 2px rgba(255,255,255,.2)",
+  },
+  menuItem: {
+    width: "100%",
+    fontSize: "1.2em",
+    fontFamily: "Anton",
+    color: "#fff",
+    opacity: 0.7,
+    textAlign: "center",
+    padding: "1em 2em",
+    cursor: "pointer",
+    "&:hover": {
+      opacity: 1,
+      backgroundColor: "rgba(255,255,255, .1)",
+    },
+  },
 });
-export default function SimpleMenu(props) {
+export default function UserMenu(props) {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = useState(null);
+  const [show, setShow] = useState(false);
+  const ref = useRef(null);
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+  const handleClick = () => {
+    setShow((prev) => !prev);
   };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  useEffect(() => {
+    const handleClose = (e) => {
+      if (ref.current && !ref.current.contains(e.target)) {
+        setShow(false);
+      }
+    };
+    document.addEventListener("click", handleClose, false);
+    return () => {
+      document.removeEventListener("click", handleClose, false);
+    };
+  }, []);
 
   return (
-    <div>
+    <div style={{ position: "relative", height: "100%" }} ref={ref}>
       <Button
         aria-controls="simple-menu"
         aria-haspopup="true"
         className={classes.button}
         onClick={handleClick}
       >
-        Open Menu
+        zxw880507@gmail.com
       </Button>
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose}>My favorates</MenuItem>
-        <MenuItem
-          onClick={() => {
-            props.userLogout();
-            handleClose();
-          }}
-        >
-          Logout
-        </MenuItem>
-      </Menu>
+      {show && (
+        <div className={classes.menuContainer}>
+          <div className={classes.menuItem}>My favorates</div>
+          <div
+            className={classes.menuItem}
+            onClick={() => {
+              handleClick();
+              props.userLogout();
+            }}
+          >
+            Log out
+          </div>
+        </div>
+      )}
     </div>
   );
 }
