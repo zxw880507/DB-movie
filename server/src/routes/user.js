f8e2071ccf996eb6e1cfef4642aa9281f97e4197
Calvin <zxw880507@gmail.com>
2021-08-19 14:24:05 -0600

    import model User

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,43 +1,42 @@
 const router = require("express").Router();
+const User = require("../db/models/user");
 
-module.exports = (db) => {
-  const { user } = db.models;
-  router.post("/register", (req, res) => {
-    const { email, password } = req.body;
-    user
-      .create({ email, password })
-      .then((data) => {
-        console.log("user registered!!!!");
-        req.session.email = data.dataValues.email;
-        res.sendStatus(200);
-      })
-      .catch((err) => {
-        console.log(err.errors[0].message);
-        res.status(400).send({ msg: err.errors[0].message });
-      });
-  });
+router.post("/register", (req, res) => {
+  const { email, password } = req.body;
+  User.create({ email, password })
+    .then((user) => {
+      console.log("user registered!!!!");
+      req.session.email = user.email;
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log(err.errors[0].message);
+      res.status(400).send({ msg: err.errors[0].message });
+    });
+});
 
-  router.get("/login", (req, res) => {
-    const email = req.session.email;
-    if (!email) {
-      res.status(400);
-      return;
-    }
-    user
-      .findAll({
-        where: {
-          email,
-        },
-      })
-      .then((data) => {
-        res.sendStatus(200);
-      });
-  });
-
-  router.post("/logout", (req, res) => {
-    req.session = null;
-    console.log("Logout successfully!!!!");
+router.get("/login", (req, res) => {
+  const email = req.session.email;
+  if (!email) {
+    const a = new User();
+    a.comparePassword();
+    res.sendStatus(400);
+    return;
+  }
+  User.findOne({
+    where: {
+      email,
+    },
+  }).then((user) => {
+    console.log(user.email);
     res.sendStatus(200);
   });
-  return router;
-};
+});
+
+router.post("/logout", (req, res) => {
+  req.session = null;
+  console.log("Logout successfully!!!!");
+  res.sendStatus(200);
+});
+
+module.exports = router;
