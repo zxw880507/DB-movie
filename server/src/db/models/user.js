66090f491041d2fc26a358b9917a24f2c84bf915
Calvin <zxw880507@gmail.com>
2021-08-19 16:56:02 -0600

    add hooks & fn to user model

--- server/src/db/models/user.js
+++ server/src/db/models/user.js
@@ -1,26 +1,37 @@
 const { Sequelize } = require("sequelize");
 const db = require("../db");
+const bcrypt = require("bcrypt");
 
-const User = db.define("user", {
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: {
-      args: true,
-      msg: "This email has been registered already!",
+const User = db.define(
+  "user",
+  {
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: "This email has been registered already!",
+      },
+      get() {
+        return this.getDataValue("email");
+      },
     },
-    get() {
-      return this.getDataValue("email");
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
     },
   },
-  password: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-});
+  {
+    hooks: {
+      beforeCreate: (user) => {
+        user.password = bcrypt.hashSync(user.password, 10);
+      },
+    },
+  }
+);
 
-User.prototype.comparePassword = () => {
-  console.log(this);
+User.prototype.comparePassword = function (plainPassword) {
+  return bcrypt.compareSync(plainPassword, this.password);
 };
 
 module.exports = User;
