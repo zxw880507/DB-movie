319839c870646f5d68169c0c6fe399af855013a8
Calvin <zxw880507@gmail.com>
2021-08-19 18:01:16 -0600

    add event for login

--- client/src/components/Login/LoginForm.js
+++ client/src/components/Login/LoginForm.js
@@ -1,18 +1,39 @@
 import React from "react";
 import { Form, Button } from "react-bootstrap";
-export default function LoginForm() {
+import useLogin from "../../hooks/useLogin";
+export default function LoginForm(props) {
+  const { alerts, changeInput, resetAlerts, login } = useLogin(props);
   return (
-    <Form className="form-card">
+    <Form className="form-card" onSubmit={login}>
       <Form.Group className="mb-3" controlId="login-email">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" autoComplete="off"/>
-        <Form.Text className="text-muted">Email is invalid</Form.Text>
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          autoComplete="off"
+          data-type="email"
+          onChange={changeInput}
+          onFocus={resetAlerts}
+        />
+        {alerts.email && (
+          <Form.Text className="text-muted">{alerts.email}</Form.Text>
+        )}
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="login-password">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" autoComplete="off"/>
-        <Form.Text className="text-muted">Password is incorrect</Form.Text>
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          autoComplete="off"
+          data-type="password"
+          onChange={changeInput}
+          onFocus={resetAlerts}
+        />
+
+        {alerts.password && (
+          <Form.Text className="text-muted">{alerts.password}</Form.Text>
+        )}
       </Form.Group>
       <Button variant="primary" type="submit" className="form-submit-button">
         Log in
