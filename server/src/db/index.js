13e831500d83bf2fbd03cb909c6faec051a3dfed
Calvin <zxw880507@gmail.com>
2021-08-19 18:00:10 -0600

    import seeds

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,10 +1,14 @@
 const db = require("./db");
-const User = require("./models/user");
+require("./models/user");
+const seeds = require("./seeds");
 
 db.authenticate()
   .then(() => {
     console.log("Connection has been established successfully.");
-    User.sync({ force: true }).then(() => console.log("db synced!!!"));
+    db.sync({ force: true }).then(() => {
+      console.log("db synced!!!");
+      seeds();
+    });
   })
   .catch((err) => {
     console.error("Unable to connect to the database:", err);
