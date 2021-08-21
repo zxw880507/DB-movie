0fb4a28bec1f3d8481dcb36c0dcb676f3f442ddd
Calvin <zxw880507@gmail.com>
2021-08-21 01:47:45 -0600

    create auth provider

---
+++ client/src/hooks/useProvideAuth.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useProvideAuth() {
+  const [showLogin, setShowLogin] = useState(false);
+  const [authState, setAuthState] = useState({
+    isAuth: false,
+    user: null,
+  });
+
+  useEffect(() => {
+    axios.get("/user/login").then((res) => {
+      if (res.data.email) {
+        setAuthState({ isAuth: true, user: res.data });
+      }
+    });
+  }, []);
+
+  const toggleLoginWindow = () => {
+    if (showLogin) {
+      setShowLogin(false);
+    } else {
+      setShowLogin(true);
+    }
+  };
+
+  const userLogin = (data) => {
+    setAuthState({ isAuth: true, user: data });
+    toggleLoginWindow();
+  };
+
+  const userLogout = () => {
+    axios.post("/user/logout").then(() => {
+      setAuthState({ isAuth: false, user: null });
+    });
+  };
+
+  return { showLogin, authState, toggleLoginWindow, userLogin, userLogout };
+}
