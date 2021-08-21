63d9fc478acc5d6e72b0125d4487eb2c70d08197
Calvin <zxw880507@gmail.com>
2021-08-21 01:48:56 -0600

    import auth context

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -5,6 +5,7 @@ import LoginForm from "./LoginForm";
 import "./login.css";
 import RegisterForm from "./RegisterForm";
 import classNames from "classnames";
+import { useAuth } from "../../hooks/providers/Auth";
 
 const useStyles = makeStyles({
   button: {
@@ -21,8 +22,9 @@ const useStyles = makeStyles({
     zIndex: 1,
   },
 });
-export default function Login(props) {
+export default function Login() {
   const classes = useStyles();
+  const { showLogin, toggleLoginWindow } = useAuth();
   const [tab, setTab] = useState("login");
   const tabsClass = classNames("login-tabs", `${tab}-tab-selected`);
   const onChange = (e) => {
@@ -30,29 +32,31 @@ export default function Login(props) {
   };
 
   return (
-    <div className="login-window">
-      <div className="login-container">
-        <IconButton className={classes.button} onClick={props.closeLogin}>
-          <HighlightOffOutlinedIcon fontSize="large" />
-        </IconButton>
-        <div className="login-heading-box">
-          <h1 className="login-heading-title">WELCOME TO</h1>
-          <p>DB MOVIE</p>
-        </div>
-        <div className="login-content-box">
-          <div className={tabsClass} onClick={onChange}>
-            <span data-value="login" className="login-tab">
-              Login
-            </span>
-            <span data-value="register" className="register-tab">
-              Register
-            </span>
+    showLogin && (
+      <div className="login-window">
+        <div className="login-container">
+          <IconButton className={classes.button} onClick={toggleLoginWindow}>
+            <HighlightOffOutlinedIcon fontSize="large" />
+          </IconButton>
+          <div className="login-heading-box">
+            <h1 className="login-heading-title">WELCOME TO</h1>
+            <p>DB MOVIE</p>
           </div>
-          <div className="form-container">
-            {tab === "login" ? <LoginForm /> : <RegisterForm />}
+          <div className="login-content-box">
+            <div className={tabsClass} onClick={onChange}>
+              <span data-value="login" className="login-tab">
+                Login
+              </span>
+              <span data-value="register" className="register-tab">
+                Register
+              </span>
+            </div>
+            <div className="form-container">
+              {tab === "login" ? <LoginForm /> : <RegisterForm />}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
