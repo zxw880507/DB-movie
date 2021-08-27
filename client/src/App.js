d4f77a2750d98233a30913420d582176a86576e7
Calvin <zxw880507@gmail.com>
2021-08-26 19:35:55 -0600

    add private route

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,38 @@ import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import Login from "./components/Login";
 import { ProvideAuth } from "./hooks/providers/Auth";
-import { BrowserRouter as Router, Route, useLocation } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  useLocation,
+  Redirect,
+} from "react-router-dom";
+import { useAuth } from "./hooks/providers/Auth";
+import { getUsername } from "./helpers";
 
 function App() {
   return (
-    <>
+    <ProvideAuth>
       <Router>
-        <Route
-          path="/"
-          render={({ location }) => (
-            <ProvideAuth>
-              <Login />
-              <Navbar />
-              <Main location={location} />
-            </ProvideAuth>
-          )}
-        />
+        <PrivateRoute />
+        <Route path="/">
+          <Login />
+          <Navbar />
+          <Main />
+        </Route>
       </Router>
-    </>
+    </ProvideAuth>
   );
 }
+function PrivateRoute() {
+  const { isAuth, user } = useAuth().authState;
 
+  const location = useLocation();
+
+  const path =
+    location.pathname === "/" && isAuth
+      ? `/${getUsername(user.email)}`
+      : location.pathname;
+  return isAuth && <Redirect to={path} />;
+}
 export default App;
