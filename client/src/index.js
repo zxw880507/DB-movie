e9ebd075c0e4dc40e2d7a014e678093d76843c74
Calvin <zxw880507@gmail.com>
2021-09-07 19:56:57 -0600

    add store to App

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider } from "react-redux";
+import { allReducers } from "./reducers";
+
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
+const store = createStore(allReducers, composedEnhancer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
