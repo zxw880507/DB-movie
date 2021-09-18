4e9a15a7f0d4af3dee5b52c125aab52a4df51642
Calvin <zxw880507@gmail.com>
2021-09-18 16:04:07 -0600

    add more actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,15 +3,27 @@ export const setKeywords = (input) => {
   return { type: "SET_KEYWORDS", payload: input };
 };
 
+export const resetKeywords = () => {
+  return { type: "RESET_KEYWORDS" };
+};
+
 export const setResult = (results) => {
   return { type: "SET_RESULT", payload: results };
 };
 
-export const fetchResult = (page) => (dispatch, getState) => {
-  const keywords = getState().searchKeywords;
+export const setPages = (total_pages, page) => {
+  return {
+    type: "SET_PAGES",
+    payload: {
+      total_pages,
+      page,
+    },
+  };
+};
+export const fetchResult = (keywords, page) => (dispatch, getState) => {
   axios.get(`/api/search`, { params: { keywords, page } }).then((res) => {
     const { page, results, total_pages } = res.data;
     dispatch(setResult(results));
+    dispatch(setPages(total_pages, page));
   });
-  // .then((res) => console.log(res.data));
 };
